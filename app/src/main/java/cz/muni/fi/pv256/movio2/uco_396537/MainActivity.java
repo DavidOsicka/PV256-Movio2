c283fbc2aae625a6663398d28a0a83d5a49948f2
david <ddoa14@seznam.cz>
2016-09-29 14:17:09 +0200

    commit with .gitignore file

---
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_396537/MainActivity.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pv256.movio2.uco_396537;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        App app = new App();
+        app.onCreate();
+    }
+}
