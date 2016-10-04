d42de93c77bdb9bb9912db81b082dd8b9421b3e2
david <ddoa14@seznam.cz>
2016-10-04 10:16:23 +0200

    targetSdk changed, calling of App class changed changes not related to ukol_3

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_396537/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_396537/MainActivity.java
@@ -2,16 +2,24 @@ package cz.muni.fi.pv256.movio2.uco_396537;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         App app = new App();
         app.onCreate();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        Log.i("Ja som TAG", "metoda onStart");
+//    }
 }
