7dc495f957f41d2533587d104605ec907acfe3ff
david <ddoa14@seznam.cz>
2016-10-22 00:36:09 +0200

    switching btw fragments work

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_396537/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_396537/MainActivity.java
@@ -2,6 +2,9 @@ package cz.muni.fi.pv256.movio2.uco_396537;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -14,57 +17,117 @@ public class MainActivity extends AppCompatActivity {
     private static final String PREFERENCES_NAME = "pref";
     private static final String THEME_NAME = "theme";
 
+    private final ListViewFragment listViewFragment = new ListViewFragment();
+    private final DetailViewFragment detailViewFragment = new DetailViewFragment();
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        // This starts a class with strict mode
         App app = new App();
         app.onCreate();
 
         super.onCreate(savedInstanceState);
 
+        // This is used to reload theme from preferences when swithing themes
         SharedPreferences pref = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
         if(pref.getString(THEME_NAME, "").equals("AppTheme1")){
             setTheme(R.style.AppTheme1);
         } else {
             setTheme(R.style.AppTheme2);
         }
-        setContentView(R.layout.activity_main);
+
+        // If we're being restored from a previous state, then we don't need to do anything and should return or else we could end up with overlapping fragments.
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                (getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            //code for big screen (like tablet)
+            setContentView(R.layout.activity_main_large);
+            fragmentManager.beginTransaction().add(R.id.main_fragment_container, listViewFragment).commit();
+            fragmentManager.beginTransaction().add(R.id.detail_fragment_container, detailViewFragment).commit();
+
+        }else{
+            //code for small screen (like smartphone)
+            setContentView(R.layout.activity_main);
+            fragmentManager.beginTransaction().add(R.id.main_fragment_container, listViewFragment).commit();
+        }
 
 
+
+        /*
         // Check that the activity is using the layout version with the fragment_container FrameLayout
-        if (findViewById(R.id.list_fragment_container) != null && findViewById(R.id.detail_fragment_container) != null) {
+        if (findViewById(R.id.main_fragment_container) != null && findViewById(R.id.detail_fragment_container) != null) {
 
             // However, if we're being restored from a previous state, then we don't need to do anything and should return or else we could end up with overlapping fragments.
             if (savedInstanceState != null) {
                 return;
             }
 
-            // Create a new Fragment to be placed in the activity layout
+            // Create new Fragments
             ListViewFragment listViewFragment = new ListViewFragment();
-            //listViewFragment.setAppContext(this);
             DetailViewFragment detailViewFragment = new DetailViewFragment();
 
             // Check the display size
-            DisplayMetrics metrics = new DisplayMetrics();
-            getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            int width = metrics.widthPixels;
-            int height = metrics.heightPixels;
+            //DisplayMetrics metrics = new DisplayMetrics();
+            //getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            //int width = metrics.widthPixels;
+            //int height = metrics.heightPixels;
 
-            if (width > 1023 || height > 1023){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+               (getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            //if ((width > height && width > 1023) || (width < height && height > 1023)){
                 //code for big screen (like tablet)
 
+                //setContentView(R.layout.activity_main_large);
+                //FragmentManager fragmentManager = getSupportFragmentManager();
+
                 // Add the fragment to the 'fragment_container' FrameLayout
-                getSupportFragmentManager().beginTransaction().add(R.id.list_fragment_container, listViewFragment).commit();
-                getSupportFragmentManager().beginTransaction().add(R.id.detail_fragment_container, detailViewFragment).commit();
+                fragmentManager.beginTransaction().add(R.id.main_fragment_container, listViewFragment).commit();
+                fragmentManager.beginTransaction().add(R.id.detail_fragment_container, detailViewFragment).commit();
 
             }else{
                 //code for small screen (like smartphone)
 
+
+
+
                 // Add the fragment to the 'fragment_container' FrameLayout
-                getSupportFragmentManager().beginTransaction().add(R.id.list_fragment_container, listViewFragment).commit();
+                fragmentManager.beginTransaction().add(R.id.main_fragment_container, listViewFragment).commit();
             }
         }
+        */
     }
+
+
+    public void onMovieItemSelected(View view) {
+        if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                (getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            //code for big screen (like tablet)
+
+        }else{
+            //code for small screen (like smartphone)
+            fragmentManager.beginTransaction().add(R.id.main_fragment_container, detailViewFragment).commit();
+        }
+    }
+
+
+    public void backToList(View view) {
+        if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                (getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            //code for big screen (like tablet)
+
+        }else{
+            //code for small screen (like smartphone)
+            fragmentManager.beginTransaction().add(R.id.main_fragment_container, listViewFragment).commit();
+        }
+    }
+
 
     public void changeTheme(View view) {
         SharedPreferences pref = getSharedPreferences(PREFERENCES_NAME,MODE_PRIVATE);
