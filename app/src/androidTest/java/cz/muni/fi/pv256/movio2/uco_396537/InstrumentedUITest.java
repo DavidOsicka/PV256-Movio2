d9df36d38acd7d062d5ee1af350019cd8ba0002f
david <ddoa14@seznam.cz>
2017-01-12 23:59:03 +0100

    Instrumented test implemented

--- app/src/androidTest/java/cz/muni/fi/pv256/movio2/uco_396537/InstrumentedUITest.java
+++ app/src/androidTest/java/cz/muni/fi/pv256/movio2/uco_396537/InstrumentedUITest.java
@@ -1,10 +1,21 @@
 package cz.muni.fi.pv256.movio2.uco_396537;
 
+import android.content.res.Configuration;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 
 /**
  * Created by david on 12.1.17.
@@ -12,18 +23,63 @@ import org.junit.Test;
 
 public class InstrumentedUITest {
 
+
     private MainActivity mainActivity;
 
+    private boolean isTablet = false;
+
     @Rule
-    public final ActivityTestRule<MainActivity> rule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    public final ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
     public void setUp() {
         mainActivity = rule.getActivity();
+        isTablet = (mainActivity.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                    (mainActivity.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE;
     }
 
     @Test
-    public void testBigScreen() {
-        
+    public void testBigScreen() throws InterruptedException {
+        if(isTablet) {
+            onView(withId(R.id.list_view_fragment)).check(matches(isDisplayed()));
+            onView(withId(R.id.detail_view_fragment)).check(matches(isDisplayed()));
+        } else {
+            onView(withId(R.id.list_view_fragment)).check(matches(isDisplayed()));
+        }
     }
+
+    @Test
+    public void testDetailFragmentAppear() throws InterruptedException {
+        onView(withId(R.id.list_view_fragment)).check(matches(isDisplayed()));
+        onView(withId(R.id.empty_view)).check(matches(isDisplayed()));
+
+        Thread.sleep(5000);
+
+        ViewInteraction recyclerView = onView(withId(R.id.recycler_view));
+        recyclerView.check(matches(isDisplayed()));
+        recyclerView.perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.detail_view_fragment)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testShowSavedMovies() throws InterruptedException {
+        boolean downloadedMoviesShown = true;
+        Thread.sleep(5000);
+
+        try {
+            onView(withId(R.id.category_view)).check(matches(isDisplayed()));
+        } catch (Exception e) {
+            downloadedMoviesShown = false;
+        }
+
+        onView(withId(R.id.switcher)).perform(click());
+
+        if(downloadedMoviesShown) {
+            onView(withId(R.id.category_view)).check(doesNotExist());
+        } else {
+            onView(withId(R.id.category_view)).check(matches(isDisplayed()));
+        }
+    }
+
 }
+
